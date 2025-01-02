037ed4d6a380ad982094095e808be4e41fcddc1e
Cctqlhh <550160403@qq.com>
2025-01-02 21:17:04 +0800

    primes ok

--- user/primes.c
+++ user/primes.c
@@ -2,29 +2,58 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+void sieve(int*);
+
 int
 main(int argc, char **argv)
-{	
-	char buff[4];
-	int p1[2], p2[2];
-	pipe(p1);
-	pipe(p2);
-	if(fork() == 0)
-	{
-		read(p1[0], buff, 4);
-		close(p1[0]);
-		printf("%d: received %s\n", getpid(), buff);
-		write(p2[1], "pong", 4);
-		close(p2[1]);
+{	int p[2];
+	pipe(p);
+
+	if(fork() != 0){
+		close(p[0]);
+		int i;
+		for(i=2; i<=35; i++){
+			write(p[1], &i, sizeof(i));
+		}
+		i = -1;
+		write(p[1], &i, sizeof(i));
+	}
+	else{
+		close(p[1]);
+		sieve(p);
+		exit(0);
+	}
+	
+	wait(0);
+	exit(0);
+}
+
+void sieve(int pl[2]){
+	int p;
+	read(pl[0], &p, sizeof(p));
+	if(p == -1){
+		exit(0);
+	}
+	printf("prime %d\n", p);
+
+	int pr[2];
+	pipe(pr);
+	if(fork() != 0){
+		close(pr[0]);
+		int buf;
+		while(read(pl[0], &buf, sizeof(buf)) && buf!= -1){
+			if(buf % p != 0){
+				write(pr[1], &buf, sizeof(buf));
+			}
+		}
+		buf = -1;
+		write(pr[1], &buf, sizeof(buf));
+		wait(0);
 		exit(0);
 	}
 	else{
-		write(p1[1], "ping", 4);
-		close(p1[1]);
-		read(p2[0], buff, 4);
-		close(p2[0]);
-		printf("%d: received %s\n", getpid(), buff);
-		wait(0);
+		close(pr[1]);
+		close(pl[0]);
+		sieve(pr);
 	}
-	exit(0);
 }
